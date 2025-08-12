a6e52a6fa4055568f798a0c98732c8cba1c5fd51
DESKTOP-URHPD6C\Shawali <m.atta6470@gmail.com>
2025-08-12 21:18:21 +0300

    notes item list view

---
+++ lib/views/widgets/notes_item_list_view.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/views/widgets/notes_item.dart';
+
+class NotesItemListView extends StatelessWidget {
+  const NotesItemListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      padding: EdgeInsets.zero,
+      physics: BouncingScrollPhysics(),
+      itemCount: 10,
+      itemBuilder: (context, index) {
+        return Padding(
+          padding: const EdgeInsets.only(bottom: 7.0),
+          child: const NotesItem(),
+        );
+      },
+    );
+  }
+}
--- lib/views/widgets/notes_view_body.dart
+++ lib/views/widgets/notes_view_body.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/views/widgets/custom_app_bar.dart';
-import 'package:notes_app/views/widgets/notes_item.dart';
+import 'package:notes_app/views/widgets/notes_item_list_view.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
@@ -8,15 +8,20 @@ class NotesViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 24.0),
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
       child: Column(
         children: [
+          SizedBox(height: 10.0),
           const CustomAppBar(),
-          const SizedBox(height: 20),
-          const NotesItem(),
+
+          const Expanded(
+            child: Padding(
+              padding: EdgeInsets.only(top: 20.0, bottom: 20.0),
+              child: NotesItemListView(),
+            ),
+          ),
         ],
       ),
     );
   }
 }
-
